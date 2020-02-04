e549dd24fab5bd4b968b825627ec9a26c419346e
rhkwj <rhkwj@naver.com>
2020-02-04 21:14:26 +0800

    make Elsa Textfiled and auto layout

---
+++ SomethingNeverChange/AllTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AllTableViewCell.swift
+//  SomethingNeverChange
+//
+//  Created by Kim Yeon Jeong on 2020/2/4.
+//  Copyright © 2020 Kim Yeon Jeong. All rights reserved.
+//
+
+import UIKit
+
+class AllTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SomethingNeverChange/KristoffTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  KristoffTableViewCell.swift
+//  SomethingNeverChange
+//
+//  Created by Kim Yeon Jeong on 2020/2/4.
+//  Copyright © 2020 Kim Yeon Jeong. All rights reserved.
+//
+
+import UIKit
+
+class KristoffTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
